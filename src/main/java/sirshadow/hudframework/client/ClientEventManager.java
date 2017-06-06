0ca71df29ac730026ca81e9b651f3fb265a1c545
sirshadow <shadowgamerxy@gmail.com>
2017-06-06 14:21:25 +0200

    fixin' things

--- src/main/java/sirshadow/hudframework/client/ClientEventManager.java
+++ src/main/java/sirshadow/hudframework/client/ClientEventManager.java
@@ -1,11 +1,15 @@
 package sirshadow.hudframework.client;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.Sys;
 import sirshadow.hudframework.client.hud.HUDRenderHelper;
 
 /**
@@ -13,20 +17,11 @@ import sirshadow.hudframework.client.hud.HUDRenderHelper;
  */
 @SideOnly(Side.CLIENT)
 public class ClientEventManager {
-
     private Minecraft mc = FMLClientHandler.instance().getClient();
-
     @SubscribeEvent
-    public void onTick(TickEvent.RenderTickEvent event) {
-        if (event.phase.equals(TickEvent.Phase.START))
-        {
-            return;
-        }else if ((mc.inGameHasFocus || mc.currentScreen == null) && !mc.gameSettings.showDebugInfo)
-        {
-
-
-            if (!HUDRenderHelper.renderHUDElements(mc))
-            {
+    public void gameRender(RenderGameOverlayEvent.Post e) {
+        if (e.getType().equals(RenderGameOverlayEvent.ElementType.ALL)) {
+            if (!HUDRenderHelper.renderHUDElements(mc)) {
                 return;
             }
         }
